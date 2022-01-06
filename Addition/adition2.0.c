382ea23e4a6b6180a0e0763b88ef16f29e87a8e3
naveenmeesala27 <93367908+naveenmeesala27@users.noreply.github.com>
2022-01-06 13:58:58 +0530

    Add files via upload

---
+++ Addition/adition2.0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	int a,b;
+	printf("Enter 1st number: ");
+	scanf("%d",&a);
+
+	printf("enter 2nd number: ");
+	scanf("%d",&b);
+
+	printf("sum of the numbers: %d", (a+b));
+	return 0;
+}
