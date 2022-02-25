328634da45bccdeb17dd0f1021e5db76b804840e
naveenmeesala27 <93367908+naveenmeesala27@users.noreply.github.com>
2022-02-25 11:36:26 +0530

    Delete pointers.c

--- pointers.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>  
-int main(){  
-int number=50;    
-int *p;      
-p=&number;
-printf("Address of p variable is %x \n",p);     
-printf("Value of p variable is %d \n",*p);
-return 0;  
-}    
